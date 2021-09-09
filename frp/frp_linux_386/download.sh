39c20ebe31b99d3266d230ab496881d5766d6101
numen06 <numen06@sina.com>
2021-09-09 15:32:24 +0800

    修改目录

---
+++ frp/frp_linux_386/download.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+version=0.37.1
+wget -O frp_linux_amd64.tar.gz "https://github.com/fatedier/frp/releases/download/v$version/frp_$version_linux_386.tar.gz"
+tar -zxvf frp_linux_amd64.tar.gz
+mv -f frp_${version}_linux_386/* /frp/
+rm -rf frp_${version}_linux_386/
+rm -rf frp_linux_amd64.tar.gz
+chmod -R 777 /frp/
--- frp/frp_linux_amd64/download.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-version=0.37.1
-wget -O frp_linux_amd64.tar.gz "https://github.com/fatedier/frp/releases/download/v0.37.1/frp_$version_linux_amd64.tar.gz"
-tar -zxvf frp_linux_amd64.tar.gz
-mv -f frp_${version}_linux_amd64/* /frp/
-rm -rf frp_${version}_linux_amd64/
-rm -rf frp_linux_amd64.tar.gz
-chmod -R 777 /frp/
