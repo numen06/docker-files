6c470fe6ce92ace9c4605ca7354b3a1e12cef16a
numen06 <numen06@sina.com>
2021-09-09 15:39:23 +0800

    修复版本号

--- frp/frp_linux_386/download.sh
+++ frp/frp_linux_386/download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 version=0.37.1
-wget -O frp_linux_amd64.tar.gz "https://github.com/fatedier/frp/releases/download/v$version/frp_$version_linux_386.tar.gz"
-tar -zxvf frp_linux_amd64.tar.gz
+wget -O frp_linux_386.tar.gz "https://github.com/fatedier/frp/releases/download/v${version}/frp_${version}_linux_386.tar.gz"
+tar -zxvf frp_linux_386.tar.gz
 mv -f frp_${version}_linux_386/* /frp/
 rm -rf frp_${version}_linux_386/
-rm -rf frp_linux_amd64.tar.gz
+rm -rf frp_linux_386.tar.gz
 chmod -R 777 /frp/
