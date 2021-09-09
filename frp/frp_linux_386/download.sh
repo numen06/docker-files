81f5f8e554191dc3405a9c045ea87b3259906c98
numen06 <numen06@sina.com>
2021-09-09 15:42:31 +0800

    Update download.sh

--- frp/frp_linux_386/download.sh
+++ frp/frp_linux_386/download.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 version=0.37.1
-wget -O frp_linux_386.tar.gz "https://github.com/fatedier/frp/releases/download/v${version}/frp_${version}_linux_386.tar.gz"
-tar -zxvf frp_linux_386.tar.gz
+tarfile=frp.tar.gz
+wget -O ${tarfile} "https://github.com/fatedier/frp/releases/download/v${version}/frp_${version}_linux_386.tar.gz"
+tar -zxvf ${tarfile}
 mv -f frp_${version}_linux_386/* /frp/
 rm -rf frp_${version}_linux_386/
-rm -rf frp_linux_386.tar.gz
+rm -rf ${tarfile}
 chmod -R 777 /frp/
